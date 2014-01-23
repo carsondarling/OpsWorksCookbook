e4c2f13778a9877f93c21134e326e0a9f0e98746
Carson Darling <carsondarling@gmail.com>
2014-01-23 01:53:03 -0500

    Added environment variables

---
+++ example-json.js
@@ -0,0 +1,14 @@
+{
+  "deploy": {
+    "app1": {
+      "env_vars": {
+        "NODE_ENV": "production"
+      }
+    },
+    "app2": {
+      "env_vars": {
+        "NODE_ENV": "production"
+      }
+    }
+  }
+}
